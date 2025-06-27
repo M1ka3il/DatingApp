95fb12dc8cbe15fcf8c6776caac8f537a5c209b6
Michael <michaelclemensr@hotmail.com>
2025-06-27 21:37:45 +0200

    Port fix : new HTTPS Port added

--- API/obj/Release/net9.0/API.AssemblyInfo.cs
+++ API/obj/Release/net9.0/API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d364337a9c8c45ec82c346c8a858479f85f5dd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
