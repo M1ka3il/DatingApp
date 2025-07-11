b56064ea6a32089c3afa5d3790af9d2854f3db57
Michael Clemens <61214594+M1ka3il@users.noreply.github.com>
2025-07-11 17:51:05 +0200

    Update Program.cs
    updated

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+builder.Services.AddDbContext<DataContext>( opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
 
