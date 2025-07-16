33a48cdda7239038f1e9be896e3791edde64fcd2
Michael Clemens <m.clemens@intelligix.de>
2025-07-16 23:09:03 +0200

    changed guid to string for first tests

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,7 +4,10 @@ namespace API.Entities;
 
 public class AppUser
 {
-  public required Guid Id { get; set; } = Guid.NewGuid();
+  public required string Id { get; set; }
+  
+  // public required Guid Id { get; set; } = Guid.NewGuid();
+
   public required string UserName { get; set; }
   public required string EMail { get; set; } 
 }
