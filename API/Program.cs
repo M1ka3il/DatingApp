7ec3472ecaefbdd7ed4ef82052382d297cf94dc1
Michael Clemens <m.clemens@intelligix.de>
2025-07-19 00:48:43 +0200

    Added a new HTTP request pipeline

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,15 @@ builder.Services.AddDbContext<AppDbContext>( opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+app.UseCors( opt => opt.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
