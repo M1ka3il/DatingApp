10f43a35f745bdb23166c5c1c55ee75d92c44c7e
Michael Clemens <m.clemens@intelligix.de>
2025-07-19 11:19:09 +0200

    Frontend - Init

---
+++ client/src/app/app.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+
+export class App implements OnInit{
+  private http = inject(HttpClient);
+
+  protected  title = ('Dating App');
+    ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response =>  console.log(response),
+      error: error =>  console.log(error),
+      complete: () => { console.log('Request completed')} 
+  })
+}
+
+
+}
