0b0c42bafa977d31cbc5c30b00ee3abb4060b9c1
Michael <michaelclemensr@hotmail.com>
2025-07-26 23:09:31 +0200

    small changes

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,10 +6,9 @@ public class AppUser
 {
   // Es kann zu Problemen beim Abruf der bestehenden Einträge kommen, da ich dort eine String-ID verwendet habe. Muss noch deletet werden aus der SQLite tabelle
   // public required string Id { get; set; }
-  public required Guid Id { get; set; } = Guid.NewGuid();
+  public  Guid Id { get; set; } = Guid.NewGuid();
   public required string UserName { get; set; }
-  public required string Email { get; set; } 
+  public required string Email { get; set; }
   public required byte[] PasswordHash { get; set; }
   public required byte[] PasswordSalt { get; set; }
-
 }
