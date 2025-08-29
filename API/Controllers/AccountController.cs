7807f3a9b8930a24844fedab0b9a092ff273fc41
Michael Clemens <m.clemens@intelligix.de>
2025-08-29 16:32:25 +0200

    Fixed Class and Method calls

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -13,8 +13,26 @@ public class AccountController(AppDbContext context) : BaseAPIController
 
   public async Task<ActionResult<AppUser>> Register(string email, string username, string password)
   {
-    var hmac = new HMACSHA512();
-    
+    if (await UserExists(username)) return BadRequest("Username is taken");
+    if (await EmailExists(email)) return BadRequest("Email is taken");
+    using var hmac = new HMACSHA512();
+    var user = new AppUser
+    {
+      UserName = username.ToLower(),
+      Email = email.ToLower(),
+      PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+      PasswordSalt = hmac.Key
+    };
+    return user;
+  }
+
+  private async Task<bool> UserExists(string username)
+  {
+    return await context.Users.AnyAsync(x => x.UserName == username.ToLower());
+  } 
+  private async Task<bool> EmailExists(string email)
+  {
+    return await context.Users.AnyAsync(x => x.Email == email.ToLower());
   }
 
 
