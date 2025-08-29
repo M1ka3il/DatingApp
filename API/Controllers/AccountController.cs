7a1f02c9b2c2661ef1f60c021c855cc52fbd166d
Michael Clemens <m.clemens@intelligix.de>
2025-08-29 16:49:23 +0200

    Login Method + added regions

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,21 +11,6 @@ namespace API.Controllers;
 public class AccountController(AppDbContext context) : BaseAPIController
 {
   [HttpPost("register")] //api/account/register
-
-  public async Task<ActionResult<AppUser>> Register(string email, string username, string password)
-  {
-    if (await UserExists(username)) return BadRequest("Username is taken");
-    if (await EmailExists(email)) return BadRequest("Email is taken");
-    using var hmac = new HMACSHA512();
-    var user = new AppUser
-    {
-      UserName = username.ToLower(),
-      Email = email.ToLower(),
-      PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
-      PasswordSalt = hmac.Key
-    };
-    return user;
-  }
   public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
   {
     if (await UserExists(registerDTO.Username)) return BadRequest("Username is taken");
@@ -40,15 +25,30 @@ public class AccountController(AppDbContext context) : BaseAPIController
     };
     return user;
   }
-
+  #region snippet_AccountController_Exists
   private async Task<bool> UserExists(string username)
   {
     return await context.Users.AnyAsync(x => x.UserName == username.ToLower());
-  } 
+  }
   private async Task<bool> EmailExists(string email)
   {
     return await context.Users.AnyAsync(x => x.Email == email.ToLower());
   }
 
+  #endregion snippet_AccountController_Exists
+
+  [HttpPost("login")]
+  public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+  {
+    var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDTO.Email.ToLower());
+    if (user == null) return Unauthorized("Invalid email address");
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+    var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDTO.Password));
+    for (int i = 0; i < computedHash.Length; i++)
+    {
+      if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+    }
+    return user;
+  }
 
 }
