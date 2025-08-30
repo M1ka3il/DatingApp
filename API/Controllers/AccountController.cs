57dab488d3f204888aaa6699ea4a29d4de0a88cf
Michael <michaelclemensr@hotmail.com>
2025-08-30 19:24:29 +0200

    AccountController und Dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Security.Cryptography;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,12 +11,33 @@ namespace API.Controllers;
 public class AccountController(AppDbContext context) : BaseAPIController
 {
   [HttpPost("register")] //api/account/register
-
-  public async Task<ActionResult<AppUser>> Register(string email, string username, string password)
+  public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
   {
-    var hmac = new HMACSHA512();
+
+    if (await UserExists(registerDto.UserName)) return BadRequest("Username is already taken.");
     
+    if (await EmailExists(registerDto.Email)) return BadRequest("Email is already registered.");
+
+    using var hmac = new HMACSHA512();
+
+    var user = new AppUser
+    {
+      UserName = registerDto.UserName,
+      Email = registerDto.Email,
+      PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
+      PasswordSalt = hmac.Key,
+    };
+
+    context.Users.Add(user);
+    await context.SaveChangesAsync();
+    return user;
   }
-
-
+  private async Task<bool> UserExists(string userName)
+  {
+    return await context.Users.AnyAsync(x => x.UserName.ToLower() == userName.ToLower());
+  }
+  private async Task<bool> EmailExists(string email)
+  {
+    return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+  }
 }
---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDTO
+{
+  [Required]
+  [DataType(DataType.Text)]
+  public string UserName { get; set; } = "";
+
+  [Required]
+  [EmailAddress]
+  [DataType(DataType.EmailAddress)]
+  public string Email { get; set; } = "";
+
+  [Required]
+  [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+  [DataType(DataType.Password)]
+  public string Password { get; set; } = "";
+}
