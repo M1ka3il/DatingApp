5b1752dec088bb4b509b74e3f71bd6cc9d8e0dc9
Michael <michaelclemensr@hotmail.com>
2025-10-12 11:20:23 +0200

    Aktualisiere RegisterDTO: Füge Standardwerte hinzu und verbessere die Datenanmerkungen für Benutzername, E-Mail und Passwort.

--- API/DTO/RegisterDTO.cs
+++ API/DTO/RegisterDTO.cs
@@ -6,13 +6,16 @@ namespace API.DTO;
 public class RegisterDTO
 {
   [Required]
-  public string Username { get; set; }
+  [DataType(DataType.Text)]
+  public string UserName { get; set; } = "";
+
   [Required]
   [EmailAddress]
-  public string Email { get; set; }
+  [DataType(DataType.EmailAddress)]
+  public string Email { get; set; } = "";
 
   [Required]
-  [MinLength(4, ErrorMessage = "Password must be at least 4 characters")]
-  public string Password { get; set; }
-
+  [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+  [DataType(DataType.Password)]
+  public string Password { get; set; } = "";
 }
