58062fee1cd4c744976fb747d7425b76f9a43b15
Michael <michaelclemensr@hotmail.com>
2025-10-12 22:06:46 +0200

    Füge TokenService zur AccountController-Klasse hinzu und aktualisiere Rückgabewerte auf UserDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,20 +2,24 @@ using System.Security.Cryptography;
 using API.Data;
 using API.DTO;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context) : BaseAPIController
+public class AccountController(AppDbContext context, TokenService tokenService) : BaseAPIController
 {
   [HttpPost("register")] //api/account/register
-  public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+  public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
   {
     if (await UserExists(registerDTO.UserName)) return BadRequest("Username is taken");
     if (await EmailExists(registerDTO.Email)) return BadRequest("Email is taken");
     using var hmac = new HMACSHA512();
+
     var user = new AppUser
     {
       UserName = registerDTO.UserName.ToLower(),
@@ -23,7 +27,11 @@ public class AccountController(AppDbContext context) : BaseAPIController
       PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDTO.Password)),
       PasswordSalt = hmac.Key
     };
-    return user;
+    
+    context.Users.Add(user);
+    await context.SaveChangesAsync();
+
+    return user.ToUserDTO(tokenService);
   }
   
   #region snippet_AccountController_Exists
@@ -39,17 +47,19 @@ public class AccountController(AppDbContext context) : BaseAPIController
   #endregion snippet_AccountController_Exists
 
   [HttpPost("login")]
-  public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+  public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
   {
     var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDTO.Email.ToLower());
-    if (user == null) return Unauthorized("Invalid email address");
+    if (user == null)
+      return Unauthorized("Invalid email address");
+    
     using var hmac = new HMACSHA512(user.PasswordSalt);
-    var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDTO.Password));
-    for (int i = 0; i < computedHash.Length; i++)
-    {
-      if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-    }
-    return user;
+      var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDTO.Password));
+      for (int i = 0; i < computedHash.Length; i++)
+      {
+        if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+      }
+      return user.ToUserDTO(tokenService);
   }
 
 }
