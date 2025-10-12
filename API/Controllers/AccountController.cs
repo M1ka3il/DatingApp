e0c7922f32b53239df14708ecba67f847f58dfb9
Michael <michaelclemensr@hotmail.com>
2025-10-12 11:20:32 +0200

    Korrigiere den Zugriff auf den Benutzernamen im Registrierungsprozess und entferne nicht benötigte using-Anweisung.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Security.Cryptography;
 using API.Data;
 using API.DTO;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace API.Controllers;
 
@@ -13,18 +13,19 @@ public class AccountController(AppDbContext context) : BaseAPIController
   [HttpPost("register")] //api/account/register
   public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
   {
-    if (await UserExists(registerDTO.Username)) return BadRequest("Username is taken");
+    if (await UserExists(registerDTO.UserName)) return BadRequest("Username is taken");
     if (await EmailExists(registerDTO.Email)) return BadRequest("Email is taken");
     using var hmac = new HMACSHA512();
     var user = new AppUser
-    /*  */{
-      UserName = registerDTO.Username.ToLower(),
+    {
+      UserName = registerDTO.UserName.ToLower(),
       Email = registerDTO.Email.ToLower(),
       PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDTO.Password)),
       PasswordSalt = hmac.Key
     };
     return user;
   }
+  
   #region snippet_AccountController_Exists
   private async Task<bool> UserExists(string username)
   {
